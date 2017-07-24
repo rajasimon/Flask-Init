55725be9ae98285a9ae851286cb64356b01ebac6
Raja Simon <rajasimon@icloud.com>
2017-07-24 11:42:18 +0530

    Added setup file for version 0.0.0

---
+++ setup.py
@@ -0,0 +1,21 @@
+"""
+Flask-Init
+
+Easy way to create Flask Web application.
+"""
+from setuptools import setup
+
+
+setup(
+    name="Flask-Init",
+    version='0.0.0',
+    url='https://github.com/rajasimon/flask-init',
+    license='See License',
+    author='Raja Simon',
+    author_email='rajasimon@icloud.com',
+    description='Easy way to create Flask Web application',
+    zip_safe=False,
+    install_requires=[
+        'Flask',
+    ],
+)

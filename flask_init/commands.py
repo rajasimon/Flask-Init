d2caa2f8e1fa8648c3c0d4f5119f934d7eabd66e
Raja Simon <rajasimon@icloud.com>
2017-07-25 15:56:57 +0530

    Single module initial commit

--- flask_init/commands.py
+++ flask_init/commands.py
@@ -1,12 +1,32 @@
+import os
 import click
 from jinja2 import Environment, PackageLoader
 
+single_module_path = 'templates/single_module'
+package_path = 'templates/package'
+blueprints_path = 'templates/blueprints'
+
 
 @click.command()
-def cli():
+@click.option('--single-module', is_flag=True)
+@click.option('--package', is_flag=True)
+@click.option('--blueprints', is_flag=True)
+def cli(single_module, package, blueprints):
     """
     Easy way to create Flask Web application.
     """
+    click.echo(single_module)
+    click.echo(package)
+    click.echo(blueprints)
+    if single_module:
+        # import pdb; pdb.set_trace()
+        for root, dirs, files in os.walk(single_module_path):
+            for name in dirs:
+                click.echo(os.path.join(root, name))
+
+            for name in files:
+                click.echo(os.path.join(root, name))
+
     env = Environment(loader=PackageLoader('flask_init', 'templates'))
 
     # Get the templates

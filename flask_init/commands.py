6e865156750d9bbc1b54eb3a1715d98d91b297b4
Raja Simon <rajasimon@icloud.com>
2017-07-26 17:26:34 +0530

    Simple and Single Module version introduced

--- flask_init/commands.py
+++ flask_init/commands.py
@@ -1,46 +1,34 @@
 import os
 import click
-from jinja2 import Environment, PackageLoader
+import flask_init
+from flask_init.helper import creator
 
-single_module_path = 'templates/single_module'
-package_path = 'templates/package'
-blueprints_path = 'templates/blueprints'
+# Project type
+simple_module_name = 'templates/simple'
+single_module_name = 'templates/single_module'
 
 
 @click.command()
+@click.argument('project-name')
+@click.option('--simple', is_flag=True)
 @click.option('--single-module', is_flag=True)
 @click.option('--package', is_flag=True)
 @click.option('--blueprints', is_flag=True)
-def cli(single_module, package, blueprints):
+def cli(project_name, simple, single_module, package, blueprints):
     """
     Easy way to create Flask Web application.
     """
-    click.echo(single_module)
-    click.echo(package)
-    click.echo(blueprints)
+    project_path = None
+
+    if project_name:
+        if simple or single_module or package or blueprints:
+            project_path = os.path.join(os.getcwd(), project_name)
+            if not os.path.isdir(project_path):
+                os.mkdir(project_path)
+
     if single_module:
-        # import pdb; pdb.set_trace()
-        for root, dirs, files in os.walk(single_module_path):
-            for name in dirs:
-                click.echo(os.path.join(root, name))
+        creator(single_module_name, project_path)
 
-            for name in files:
-                click.echo(os.path.join(root, name))
-
-    env = Environment(loader=PackageLoader('flask_init', 'templates'))
-
-    # Get the templates
-    template = env.get_template('app.py-tpl')
-
-    # Apply context
-    parsed_template = template.render()
-
-    # Remove tpl in the name
-    file_name = template.name.split('-tpl')[0]
-    with open(file_name, "w") as f:
-        f.write(parsed_template + '\n')
-
-    # Notify user about the file
-    click.echo('Flask app.py file generated. Use below command to run.')
-    click.echo('FLASK_APP=app.py flask run')
-    return True
+    else:
+        creator(simple_module_name, project_path)
+        
